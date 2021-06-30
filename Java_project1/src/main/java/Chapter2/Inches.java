3837f4ca352851768e94ed37f7398e3c9540c26b
james-strom <james.stromnes@gmail.com>
2021-06-30 12:01:56 +0200

    Project 1 Classwork

---
+++ Java_project1/src/main/java/Chapter2/Inches.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter2;
+
+//listing 1
+/*    
+   Compute the number of cubic inches 
+   in 1 cubic mile. 
+*/    
+class Inches {    
+  public static void main(String args[]) {    
+    long ci; 
+    long im; 
+   
+    im = 5280 * 12; 
+ 
+    ci = im * im * im; 
+   
+    System.out.println("There are " + ci + 
+                       " cubic inches in cubic mile."); 
+   
+  }    
+}
