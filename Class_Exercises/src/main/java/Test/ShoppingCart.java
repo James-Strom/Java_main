e611c729d2747e03d13eaa257fc5e8d09e3d0479
james-strom <james.stromnes@gmail.com>
2021-07-08 09:26:26 +0200

    Netbeans Test 2

---
+++ Class_Exercises/src/main/java/Test/ShoppingCart.java
@@ -0,0 +1,28 @@
+package Test;
+
+import Exercise4.ex04_2_exercise.*;
+
+public class ShoppingCart {
+    
+    public static void main(String[] args) {
+        String custName = "Mary Smith";
+        String itemDesc = "Shirt";
+        String message = custName+" wants to purchase a "+itemDesc;
+        
+        // Declare and initialize numeric fields: price, tax, quantity, and total.   
+        double price=10;
+        double tax = 1.15;
+        int quantity = 2;
+        double total;
+        // Modify message to include quantity 
+        message = custName+" wants to purchase "+quantity+" "+itemDesc;
+        
+        System.out.println(message);
+        
+        // Calculate total and then print the total cost
+        total = quantity * (price * tax);
+        
+        System.out.println("Price after tax "+total);        
+    }    
+}
+
