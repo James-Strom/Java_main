22931aa81982473a1ba7dfc41aed74b0bb56fd42
james-strom <james.stromnes@gmail.com>
2021-07-21 11:39:57 +0200

    Section 4 video 46 update

---
+++ java_project_2_master/Section4/StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myVariable = 50;
+
+	    if (myVariable == 50) {
+            System.out.println("Printed");//indentation
+        }
+	    myVariable++;
+	    myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more");
+
+    }
+}
