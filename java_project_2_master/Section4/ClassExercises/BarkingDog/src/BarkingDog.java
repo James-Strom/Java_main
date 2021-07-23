a2e5e8bd8b29422bd5113211b17c043960485436
james-strom <james.stromnes@gmail.com>
2021-07-23 11:40:15 +0200

    Section 4 exercise 3 complete

--- java_project_2_master/Section4/ClassExercises/BarkingDog/src/BarkingDog.java
+++ java_project_2_master/Section4/ClassExercises/BarkingDog/src/BarkingDog.java
@@ -2,7 +2,7 @@ public class BarkingDog {
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
         if (hourOfDay > 23 || hourOfDay < 0) {
             return false;
-        } else if (hourOfDay >= 22 || hourOfDay <=8) {
+        } else if ((hourOfDay > 22 || hourOfDay <8) && barking) {
             return true;
         } else {
             return false;
--- java_project_2_master/Section4/ClassExercises/BarkingDog/src/main.java
+++ java_project_2_master/Section4/ClassExercises/BarkingDog/src/main.java
@@ -1,6 +1,8 @@
 public class main {
     public static void main(String[] args) {
         BarkingDog.shouldWakeUp(true, 1);
-        System.out.println(barking);
+        BarkingDog.shouldWakeUp(false, 6);
+        BarkingDog.shouldWakeUp(true, 10);
+        System.out.println(BarkingDog.shouldWakeUp(true,10));
     }
 }
