f39fd3ef7fa72fb61f6771f1377d80bc52298cbc
james-strom <james.stromnes@gmail.com>
2021-07-23 11:15:15 +0200

    section 4 class exercise 3 complete

---
+++ java_project_2_master/Section4/ClassExercises/BarkingDog/src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        BarkingDog.shouldWakeUp(true, 1);
+        System.out.println(barking);
+    }
+}
---
+++ java_project_2_master/Section4/ClassExercises/MegaBytesConverter/src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-5000);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2000);
+    }
+}
