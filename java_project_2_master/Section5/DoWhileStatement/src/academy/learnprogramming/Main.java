73e7776f3449a1d871ad85ea9dd097f891c86cba
james-strom <james.stromnes@gmail.com>
2021-09-02 15:18:57 +0200

    File fix up section 5

--- java_project_2_master/Section5/DoWhileStatement/src/academy/learnprogramming/Main.java
+++ java_project_2_master/Section5/DoWhileStatement/src/academy/learnprogramming/Main.java
@@ -3,6 +3,20 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int count = 0;
+        while (count != 5) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        for(int i=1; i!=6; i++) {
+            System.out.println("Count value is " + i);
+        }
+
+        count = 1;
+        do {
+            System.out.println("Count value is " + count);
+            count++;
+        } while(count != 6);
     }
 }
