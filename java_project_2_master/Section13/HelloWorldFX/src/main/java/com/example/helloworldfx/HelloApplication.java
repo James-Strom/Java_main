c0bcc8903ad961c983b45f5614cb88e09e193bdb
james-strom <james.stromnes@gmail.com>
2021-09-22 11:46:16 +0200

    section 13 draft 1

---
+++ java_project_2_master/Section13/HelloWorldFX/src/main/java/com/example/helloworldfx/HelloApplication.java
@@ -0,0 +1,34 @@
+package com.example.helloworldfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//
+//        Label greeting = new Label("Welcome to JavaFX!");
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//
+//        root.getChildren().add(greeting);
+
+        stage.setTitle("Hello JavaFX!");
+        stage.setScene( new Scene(root, 700, 240));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
