f1ccba2979bead8ae5358649fca3c1168d79d4cc
james-strom <james.stromnes@gmail.com>
2021-10-08 13:36:46 +0200

    section 13 update 4

--- java_project_2_master/Section13/Events/src/main/java/com/example/events/HelloApplication.java
+++ java_project_2_master/Section13/Events/src/main/java/com/example/events/HelloApplication.java
@@ -1,15 +1,15 @@
 package com.example.events;
 
 import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
